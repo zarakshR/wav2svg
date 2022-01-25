51915d667ecbcad950cc7b69c3e52d9712bfdf3a
Zaraksh Rahman <zarakshR@pm.me>
2022-01-25 13:36:42 +0000

    Update helper script and .gitignore

--- .source-me.sh
+++ .source-me.sh
@@ -1,4 +1,4 @@
-# Use this until a proper Makefile is built
+# Use this until a proper Makefile is written
 
 go() {
     gcc -g -o wav wav.c
@@ -9,4 +9,15 @@ go() {
 
 db() {
     gdb wav
+}
+
+# dumps all bytes in "sample.wav"
+dbytes() {
+    gcc printbytes.c -o printbytes
+    ./printbytes
+}
+
+# dumps bytes in data section of the dataChunk
+ddata() {
+    xxd -s 78 -o -78 sample.wav
 }
