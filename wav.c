8fb1bddbad4700fc7b0649c7940fd62fc597945e
Zaraksh Rahman <zarakshR@pm.me>
2022-01-27 12:23:59 +0000

    Add .clang-format. Change formatting of wav.c

--- wav.c
+++ wav.c
@@ -12,35 +12,36 @@
 // PCM data is signed 2's-complement except for resolutions of 1-8 bits, which
 // are represented as offset binary.
 
-typedef struct _FormatChunk {  // 24 bytes in length.
-    BYTE chunkID[4];           // Should be "fmt ". Note the trailing space.
+typedef struct _FormatChunk { // 24 bytes in length.
+    BYTE chunkID[4];          // Should be "fmt ". Note the trailing space.
     BYTE chunkSize[4];
-    BYTE formatCode[2];     // Should be 0x10 0x00 for PCM data
-    BYTE channels[2];       // No. of channels -- Also samples per block
-    BYTE sampleRate[4];     // Samples per second.
-    BYTE avgByteRate[4];    // Bytes per second
-    BYTE blockAlign[2];     // Bytes per block
-    BYTE bitsPerSample[2];  // Bits per sample = 8 * Bytes per sample. This
-                            // field is required for LPCM encoded data.
+    BYTE formatCode[2];    // Should be 0x10 0x00 for PCM data
+    BYTE channels[2];      // No. of channels -- Also samples per block
+    BYTE sampleRate[4];    // Samples per second.
+    BYTE avgByteRate[4];   // Bytes per second
+    BYTE blockAlign[2];    // Bytes per block
+    BYTE bitsPerSample[2]; // Bits per sample = 8 * Bytes per sample. This
+                           // field is required for LPCM encoded data.
 } FormatChunk;
 
 typedef struct _DataChunk {
-    BYTE chunkID[4];  // Should be "data"
+    BYTE chunkID[4]; // Should be "data"
     BYTE chunkSize[4];
-    BYTE* data;  // use chunkSize to malloc this
+    BYTE* data; // use chunkSize to malloc this
 } DataChunk;
 
 // Format chunk and data chunk are siblings and children of RIFF chunk
-typedef struct _RiffChunk {  // 12 bytes in length.
-    BYTE chunkID[4];         // Chunk ID. Should be "RIFF".
-    BYTE chunkSize[4];       // This includes the dataChunk.data
-    BYTE format[4];          // Wave ID. Should be "WAVE".
+typedef struct _RiffChunk { // 12 bytes in length.
+    BYTE chunkID[4];        // Chunk ID. Should be "RIFF".
+    BYTE chunkSize[4];      // This includes the dataChunk.data
+    BYTE format[4];         // Wave ID. Should be "WAVE".
     FormatChunk fmtChunk;
     DataChunk dataChunk;
 } RiffChunk;
 
 // Returns sum of first N bytes at seriesofBytes
-uint64_t sumNBytesFrom(const BYTE* seriesOfBytes, uint32_t N) {
+uint64_t sumNBytesFrom(const BYTE* seriesOfBytes, uint32_t N)
+{
     uint64_t sum = 0;
     for (int64_t _i = (N - 1); _i >= 0; _i--) {
         sum = sum << 8 | *(seriesOfBytes + _i);
@@ -48,13 +49,12 @@ uint64_t sumNBytesFrom(const BYTE* seriesOfBytes, uint32_t N) {
     return sum;
 }
 
-int main() {
-    int8_t _i;  // Index variable for loop. Should not be unsigned
+int main()
+{
+    size_t _i; // Index variable for loops.
 
     FILE* input_file = fopen("sample.wav", "rb");
-    if (input_file == NULL) {
-        return 4;
-    }
+    if (input_file == NULL) { return 4; }
 
     RiffChunk riff_chunk;
 
@@ -64,22 +64,20 @@ int main() {
     fread(&riff_chunk.fmtChunk, sizeof(riff_chunk.fmtChunk), 1, input_file);
 
     // This is the format code for linear PCM encoding
-    BYTE fmt_pcm_sig[2] = {0x10, 0x00};
+    BYTE fmt_pcm_sig[2] = { 0x10, 0x00 };
 
     // Make sure that file is a WAVE RIFF file with linear PCM encoding.
     BYTE signal = 0;
 
     // Check if RIFF
-    signal += (memcmp(riff_chunk.chunkID, "RIFF", BYTE_SIZE * 4));
+    signal += (memcmp(riff_chunk.chunkID, "RIFF", sizeof(BYTE) * 4));
     // Check if WAVE
-    signal += (memcmp(riff_chunk.format, "WAVE", BYTE_SIZE * 4));
+    signal += (memcmp(riff_chunk.format, "WAVE", sizeof(BYTE) * 4));
     // Check if using linear PCM encoding
     signal += (memcmp(riff_chunk.fmtChunk.formatCode, fmt_pcm_sig,
                       sizeof(fmt_pcm_sig)));
 
-    if (signal != 0) {
-        return 1;
-    }
+    if (signal != 0) { return 1; }
 
     // Read file until "data" bytes are found
     // We have to read manually in groups of 4 bytes starting from every
@@ -88,10 +86,8 @@ int main() {
     BYTE buf[4];
     for (;;) {
         fread(&buf, 4, 1, input_file);
-        if ((memcmp(&buf, "data", 4)) == 0) {  // "data" found
-            break;
-        }
-        fseek(input_file, -3, SEEK_CUR);  // Go back 3 bytes
+        if ((memcmp(&buf, "data", 4)) == 0) { break; } // "data" found
+        fseek(input_file, -3, SEEK_CUR);               // Go back 3 bytes
     }
     // Write "data" into chunkID
     strcpy(riff_chunk.dataChunk.chunkID, buf);
@@ -105,28 +101,26 @@ int main() {
 
     // Allocate data_size bytes of data for dataChunk.data
     riff_chunk.dataChunk.data = (BYTE*)malloc(data_size);
-    if (riff_chunk.dataChunk.data == NULL) {
-        return 2;
-    }
+    if (riff_chunk.dataChunk.data == NULL) { return 2; }
 
     // Read data_size bytes of data into dataChunk.data
     fread(riff_chunk.dataChunk.data, data_size, 1, input_file);
-    if (feof(input_file) || ferror(input_file)) {
-        return 3;
-    }
-    fclose(input_file);  // Close file since nothing left to be read from it.
+    if (feof(input_file) || ferror(input_file)) { return 3; }
+
+    // Close file since nothing left to be read from it.
+    fclose(input_file);
 
     // Create block and sample structures to represent raw PCM data
     // Calculate bits and bytes per sample
-    uint16_t bits_per_sample =
-        sumNBytesFrom(riff_chunk.fmtChunk.bitsPerSample, 2);
+    uint16_t bits_per_sample
+        = sumNBytesFrom(riff_chunk.fmtChunk.bitsPerSample, 2);
     uint16_t bytes_per_sample = (bits_per_sample / 8);
 
     // A struct Sample represents the instantaneous sound data for one
     // channel
     typedef struct {
-        BYTE byte[bytes_per_sample];  // LSB-MSB repr. of
-                                      // instantaenous amplitude
+        // LSB-MSB repr. of instantaenous amplitude
+        BYTE byte[bytes_per_sample];
     } Sample;
 
     // Calculate no. of samples per block which is equal to the no. of
@@ -136,7 +130,8 @@ int main() {
     // A struct block represents the instantaneous sound data for all
     // channels
     typedef struct {
-        Sample sample[samples_per_block];  // 1 sample for each channel.
+        // 1 sample for each channel.
+        Sample sample[samples_per_block];
     } Block;
 
     // We can figure out the total no. of blocks since we have the total
@@ -163,13 +158,15 @@ int main() {
     // Loop over each block
     for (block_index = 0; block_index < block_count; block_index++) {
         block_offset = block_index * bytes_per_block;
+
         // Loop over each sample in the block
         for (sample_index = 0; sample_index < samples_per_block;
              sample_index++) {
             sample_offset = sample_index * bytes_per_sample;
+
             // Loop over each byte in the sample
             for (byte_index = 0; byte_index < bytes_per_sample; byte_index++) {
-                byte_offset = byte_index;  // Trivial, included for clarity
+                byte_offset = byte_index; // Trivial, included for clarity
 
                 byte = *(data_ptr + block_offset + sample_offset + byte_offset);
                 blockbuf.sample[sample_index].byte[byte_index] = byte;
@@ -208,7 +205,7 @@ for (; pos < end_pos; pos) {
 IV. Print the blockbuf in the loop used to read PCM data. Useful for inspecting
     which bytes we are actually reading. This is only for 2 sample 2 byte (i.e.,
     2 channel, 16-bit PCM data) Blocks but the 0/1 values can be swapped out for
-any other block size.
+    any other block size.
 
 printf("%02x:\t", (blockAlign * block_index));
 printf("%02x", blockbuf.sample[0].byte[0]);
