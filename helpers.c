f82649e3265cf36956166a802be38baff01067f3
Zaraksh Rahman <zarakshR@pm.me>
2022-01-28 22:47:27 +0000

    efactor sizeOfNBytesAt() to use size_t
    Changed it so that the _i loop variable uses size_t. Adjusted the
    pointer offset and for loop parameters to reflect this.

--- helpers.c
+++ helpers.c
@@ -7,8 +7,8 @@ uint64_t sumNBytesFrom(const BYTE* seriesOfBytes, uint32_t N)
     if (seriesOfBytes == NULL) { return 0; }
 
     uint64_t sum = 0;
-    for (int64_t _i = (N - 1); _i >= 0; _i--) {
-        sum = sum << 8 | *(seriesOfBytes + _i);
+    for (size_t _i = N; _i > 0; _i--) {
+        sum = sum << 8 | *(seriesOfBytes + _i - 1);
     }
     return sum;
 }
