b168949593535fecffc7ad8cffc2da60a3b37096
Zaraksh Rahman <zarakshR@pm.me>
2022-01-31 22:28:42 +0000

    Changed strcpy() to strncpy() in parser.c
    This is much safer. Previously, strcpy kept copying until it hit the
    first null byte it encountered, which was usually the first byte of the
    data section - if the first byte of the data section was not null it
    would keep copying until it hit one which could be arbitrarily large
    causing buffer overflows. Now strnpy() only ever copies 4 bytes.

--- parser.c
+++ parser.c
@@ -44,7 +44,7 @@ MasterChunk* parseFile(FILE* input_file)
         fseek(input_file, -3, SEEK_CUR);
     }
     // Write "data" into chunkID.
-    strcpy(master_chunk.dataChunk.chunkID, buf);
+    strncpy(master_chunk.dataChunk.chunkID, buf, 4);
 
     // Read dataChunk.chunkSize. This is the total no. of PCM data bytes.
     fread(&master_chunk.dataChunk.chunkSize, 4, 1, input_file);
